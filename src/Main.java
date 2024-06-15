e6e991841a54ab663ab13a5c5a436d4e959e411b
eduardovalias <edupv123@hotmail.com>
2024-06-15 17:21:53 -0300

    Testes com a main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
-        }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Biblioteca biblioteca = new Biblioteca();
+        Livro livro = new Livro("Harry Potter", "JK. Rowling", "123", 2002);
+
+        biblioteca.adicionarLivro(livro);
+        List<Livro> teste = biblioteca.listarLivros();
     }
 }
