f85153ac08e6ea9afcf778fa87f9ce52bd7033e8
eduardovalias <edupv123@hotmail.com>
2024-06-15 17:08:13 -0300

    Adicionar classe livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,2 +1,45 @@
 public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, String isbn, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
 }
