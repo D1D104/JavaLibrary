2dac7aec5c5e8247c19e4f73036c3fbf50d5c88e
Diego <diegoresende403@gmail.com>
2024-06-16 01:37:38 -0300

    Ajustar implementações na main junto com funcionalidades do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        }
     }
 }
